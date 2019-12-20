1fc8837aa11e7d6b54eb3fc4f2819ce8375173a6
chendongpu <2323178881@qq.com>
2019-12-20 11:25:17 +0800

    仿springbucks项目jpa访问mysql生成订单

---
+++ springbucks/src/main/java/com/example/springbucks/repository/CoffeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.springbucks.repository;
+
+import com.example.springbucks.model.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoffeeRepository extends JpaRepository<Coffee,Long> {
+
+}

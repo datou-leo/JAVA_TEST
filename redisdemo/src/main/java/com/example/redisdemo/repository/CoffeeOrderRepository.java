773b0c2c0046bedeee07563260accced2084a75f
chendongpu <2323178881@qq.com>
2019-12-23 10:30:59 +0800

    coffeebucks 项目结合mysql+redis

---
+++ redisdemo/src/main/java/com/example/redisdemo/repository/CoffeeOrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.redisdemo.repository;
+
+import com.example.redisdemo.model.CoffeeOrder;
+import com.example.redisdemo.model.CoffeeOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoffeeOrderRepository extends JpaRepository<CoffeeOrder,Long> {
+
+}
---
+++ redisdemo/src/main/java/com/example/redisdemo/repository/CoffeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.redisdemo.repository;
+
+import com.example.redisdemo.model.Coffee;
+import com.example.redisdemo.model.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoffeeRepository extends JpaRepository<Coffee,Long> {
+
+}
